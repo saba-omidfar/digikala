35255ab756959907e35f26651efb3a6570b65167
saba omidfar <saba.omidfar68@gmail.com>
2024-11-03 16:36:21 +0330

    update backend

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
+require('dotenv').config();
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const authRouter = require("./routes/authRouter");
@@ -16,14 +16,15 @@ const adsRouter = require("./routes/adsRouter");
 const popularBrandsRouter = require("./routes/popularBrandsRouter");
 const commentsRouter = require("./routes/commentsRouter");
 const sellerRouter = require("./routes/sellerRouter");
-const productsUserRouter = require("./routes/usersRouter");
+const productsUserRouter = require("./routes/productsUserRouter"); // تغییر نام
 const basketRouter = require("./routes/basketRouter");
 const userProductsViewRouter = require("./routes/userProductViewRouter");
 
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json()); // استفاده از express.json() به جای bodyParser
+
 
 app.use("/auth", authRouter);
 app.use("/users", usersRouter);
@@ -38,10 +39,20 @@ app.use("/ads", adsRouter);
 app.use("/popular-brands", popularBrandsRouter);
 app.use("/comments", commentsRouter);
 app.use("/seller", sellerRouter);
-app.use("/users", productsUserRouter);
+app.use("/products-user", productsUserRouter); // تغییر نام
 app.use("/basket", basketRouter);
 app.use("/user", userProductsViewRouter);
 
-mongoose.connect("mongodb://127.0.0.1:27017/digikala");
-mongoose.Promise = global.Promise;
-app.listen(5000);
+// اتصال به MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB Atlas');
+    const port = process.env.PORT || 5000;
+    app.listen(port, '0.0.0.0', () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch(err => {
+    console.error('Error connecting to MongoDB Atlas', err);
+    process.exit(1); // خروج از برنامه در صورت بروز خطا
+  });
